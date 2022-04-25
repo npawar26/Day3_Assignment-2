d224c08355806cdcc652b009fb00d091c8bec3a6
npawar26 <nikhilpwar123@gmail.com>
2022-04-25 20:50:00 +0530

    Adding- Added Employee Welcome

---
+++ EmpWelcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Day3Assignment;
+
+public class EmpWelcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
